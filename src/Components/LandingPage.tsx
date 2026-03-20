5a6fd501cf3b00eb3451519fb7543d4840c46dc1
aman <aman.panchal@psquarecompany.com>
2026-03-20 15:48:04 +0530

    Home Page sections

---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,24 @@
+
+
+const LandingPage = () => {
+  return (
+    <div>
+        <div className="upper">
+
+        </div>
+        <div className="midle">
+
+        </div>
+        <div className="lower">
+            
+        </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default LandingPage
